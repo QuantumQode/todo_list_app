169154723459d1cadd7f738a7dd17d7fb416f4bf
Pouria <pouria.ghasemi@yahoo.co.uk>
2025-01-09 18:00:47 +0000

    basic login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <h1>Login</h1>
+      <input type="text"/>
+      <input type="password"/>
+      <button>Submit</button>
     </div>
   );
 }
