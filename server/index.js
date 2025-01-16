8a1ad9cb04a98a4571c5529c0088bf5fb8c5cc43
Pouria <pouria.ghasemi@yahoo.co.uk>
2025-01-16 10:14:52 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,6 +11,8 @@ const cors = require('cors');
 app.use(express.json()); 
 app.use(cors());
 
+
+//create connection to database
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -18,6 +20,7 @@ const db = mysql.createConnection({
     database: 'todo-list'
 });
 
+//connect to database
 db.connect((err) => {
     if (err) {
         console.error('Error connecting to the database:', err);
@@ -25,7 +28,7 @@ db.connect((err) => {
     }
     console.log('Connected to the database');
 
-    // Create users table if it doesn't exist
+    // Create a variable that contains the SQL to creaate users table if it doesn't exist
     const createUsersTableQuery = `
         CREATE TABLE IF NOT EXISTS users (
             id INT AUTO_INCREMENT PRIMARY KEY,
@@ -33,6 +36,7 @@ db.connect((err) => {
             userPassword VARCHAR(255) NOT NULL
         )
     `;
+    //create table
     db.query(createUsersTableQuery, (err, result) => {
         if (err) {
             console.error('Error creating users table:', err);
@@ -42,10 +46,12 @@ db.connect((err) => {
     });
 });
 
+//create a post endpoint for user registration
 app.post('/register', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
 
+    //insert user into database
     db.query(
         'INSERT INTO users (userName, userPassword) VALUES (?, ?)',
         [username, password],
